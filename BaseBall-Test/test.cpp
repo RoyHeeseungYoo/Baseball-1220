fc9e544148063f9e42ce5ec6bef0204fef1c23ea
Heeseung Yoo <heeseung.yoo@samsung.com>
2024-07-03 14:35:26 +0900

    [feature] 1

--- BaseBall-Test/test.cpp
+++ BaseBall-Test/test.cpp
@@ -1,6 +1,10 @@
 #include "pch.h"
+#include <stdexcept>
 #include "../BaseBallKata/baseball.cpp"
 
-TEST(BaseballGame, TryGameTest) {
-  EXPECT_EQ(1, 1);
+using namespace std;
+
+TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmatched) {
+	BaseBall game;
+	EXPECT_THROW(game.guess(string("12")), length_error);
 }
--- BaseBallKata/baseball.cpp
+++ BaseBallKata/baseball.cpp
@@ -1,4 +1,10 @@
+#include <stdexcept>
+
+using namespace std;
+
 class BaseBall {
 public:
-
+	void guess(const string& string) {
+		throw length_error("Must be three letters.");
+	}
 };
